3a332b276e48380c58a00fc4a4df8bdbeabb27c4
Todd <qq199407231031>
2024-11-08 10:32:41 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # PlanXSDK-Go
 Here is the go version of PlanXSDK.
+
+## First step:
+> * Go to the **[PlanX CP](https://cp.planx.io)** to register.
+> * Get **AppId** and **SecretKey**.
+## Second step:
+> * Download the package into your project via the command:
+```
+    go get github.com/PlanXDev/planx-sdk-core-go
+```
+## Example Usage
+> * This example get balance information for all token accounts:
+```go
+    baseUrl := "https://cp-api.planx.io"
+    appId := "Go to the PlanX CP website to get"
+    secretKey := "Go to the PlanX CP website to get"
+    client, _ := api.NewClientWithOptions(core.NewConfig(), credential.NewAccessKeyCredential(baseUrl, appId, secretKey))
+    base, err := client.GetAccountBalancesBase(api.CreateGetAccountBalancesBaseRequest())
+    if err != nil {
+    println(err.Error())
+    }
+    println(base.String())
+    
+    // Process your bussiness ...
+```
+
+## Other
+> * Supports Go 1.17+
--- client_test.go
+++ client_test.go
@@ -1,7 +1,6 @@
 package planx_sdk_core_go
 
 import (
-	"encoding/json"
 	"github.com/PlanXDev/planx-sdk-core-go/api"
 	"github.com/PlanXDev/planx-sdk-core-go/core"
 	"github.com/PlanXDev/planx-sdk-core-go/core/credential"
@@ -9,29 +8,15 @@ import (
 )
 
 func TestClient(t *testing.T) {
-	config := core.NewConfig()
-	config.AutoRetry = true
-	accessKeyCredential := credential.NewAccessKeyCredential(
-		"http://192.168.1.200:8886",
-		"cg7Zqpb4",
-		"842c2d804f5b5e9bba7a35436d9585b26bca4c69",
-	)
-	client, _ := api.NewClientWithOptions(config, accessKeyCredential)
-	client.OpenLogger()
-	request := api.CreateGetGiftSourceInfoBatchRequest([]string{
-		"1E44BA0A-6D95-450B-9D0D-9F934FC5EFCA",
-		"215FF713-D8C5-44DD-9597-9CCD722BE39E",
-		"498DDBA1-E4AD-45CD-A9A1-27A11DDC201B",
-	})
-	response, err := client.GetGiftSourceInfoBatch(request)
+	baseUrl := "https://cp-api.planx.io"
+	appId := "Go to the PlanX CP website to get"
+	secretKey := "Go to the PlanX CP website to get"
+	client, _ := api.NewClientWithOptions(core.NewConfig(), credential.NewAccessKeyCredential(baseUrl, appId, secretKey))
+	base, err := client.GetAccountBalancesBase(api.CreateGetAccountBalancesBaseRequest())
 	if err != nil {
 		println(err.Error())
-	} else {
-		if len(response.Data) > 0 {
-			for _, datum := range response.Data {
-				marshal, _ := json.Marshal(datum)
-				println(string(marshal))
-			}
-		}
 	}
+	println(base.String())
+
+	// Process your bussiness ...
 }
